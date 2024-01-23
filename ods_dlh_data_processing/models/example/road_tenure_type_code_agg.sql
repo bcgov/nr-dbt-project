aa8c55f86e198eef55567438f20115fa273fc006
vishreddy01 <vish.reddy@gov.bc.ca>
2024-01-23 13:40:00 -0800

    Added Analytical Function

--- ods_dlh_data_processing/models/example/road_tenure_type_code_agg.sql
+++ ods_dlh_data_processing/models/example/road_tenure_type_code_agg.sql
@@ -12,7 +12,7 @@
 with source_data as (
 
     --select a.*,26 as Total_Count from fdw_ods_rrs_replication.road_tenure_type_code a
-    select a.*,26 as Total_Count from {{ var('db_fdw_rrs') }}.road_tenure_type_code a
+    select a.*,count(*) over (partition by expiry_date) as Total_Count from {{ var('db_fdw_rrs') }}.road_tenure_type_code a
 
 )
 
